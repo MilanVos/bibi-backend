ecb6af816254315062f71e1bb385dcacacfb3f7f
MultiTime <MilanV2005@outlook.com>
2026-04-20 01:36:24 +0200

    fix: CORS voor jongerenraadd.nl

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express()
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'http://jongerenraadd.nl',
+  'https://jongerenraadd.nl',
+  'http://www.jongerenraadd.nl',
+  'https://www.jongerenraadd.nl',
   process.env.FRONTEND_URL
 ].filter(Boolean)
 
